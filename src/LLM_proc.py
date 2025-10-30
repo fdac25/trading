078dbd8b2cce56f60ccf0691a23bdbe36fbdbf2c
gdelellis-2 <gevans16@utk.edu>
2025-10-29 22:40:58 -0400

    Create LLM_proc.py

---
+++ src/LLM_proc.py
@@ -0,0 +1,7 @@
+"""
+SKELETON CODE
+ - use the LLM https://medium.com/@tejasdalvi927/sentiment-analysis-using-tf-idf-and-logisticregression-5ccc4f5c4f81 as a non
+ trivial solution to analyzing the contents of the articles to get a parity score
+ - That particular application is made to generate a negative, positive, or neutral score based on the text (I believe it's pretrained)
+ - Will add actual code soon, still making sure that I understand the function of the package correctly
+"""
